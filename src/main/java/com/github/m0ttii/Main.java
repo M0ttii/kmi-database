23612731e6d9ffbb51a105289c5417832f22a1f0
Adrian <adrian.doernberg@berlin.de>
2024-05-30 20:45:32 +0200

    f

--- src/main/java/com/github/m0ttii/Main.java
+++ src/main/java/com/github/m0ttii/Main.java
@@ -1,46 +1,12 @@
 package com.github.m0ttii;
 
-import com.github.m0ttii.repository.RepositoryProxy;
-import com.github.m0ttii.test.*;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.List;
-
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver");
 
-        EmployeeRepository employeeRepository = RepositoryProxy.newInstance(EmployeeRepository.class);
-        KundeRepository kundeRepository = RepositoryProxy.newInstance(KundeRepository.class);
-        AdresseRepository adresseRepository = RepositoryProxy.newInstance(AdresseRepository.class);
 
-        try{
-
-            AdresseEntity adresseEntity = adresseRepository.findAll().where("stadt", "Berlin");
-            KundeEntity kundeEntity = new KundeEntity();
-            kundeEntity.name = "Kunde";
-            kundeRepository.insert(kundeEntity);
-
-            kundeEntity.adresse = adresseEntity;
-            kundeRepository.update(kundeEntity);
-            //AdresseEntity adresseEntity = adresseRepository.findById(1).findOne();
-//            KundeEntity kundeEntity = kundeRepository.findById(1).findOne();
-//            System.out.println(kundeEntity.name);
-//            kundeEntity.adresse.stadt = "Berlin2";
-//            kundeRepository.update(kundeEntity);
-
-
-//                EmployeeEntity employeeEntitie = employeeRepository.findById(1).findOne();
-//                System.out.println(employeeEntitie.department.name);
-
-        }catch (Exception ex){
-            System.out.println(ex);
-        }
     }
 
 
