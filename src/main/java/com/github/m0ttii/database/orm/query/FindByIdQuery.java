513bbf8a70b5eccdbe5a9be11a9afb68c0a1addd
Adrian <adrian.doernberg@berlin.de>
2024-06-25 13:11:33 +0200

    j

--- src/main/java/com/github/m0ttii/database/orm/query/FindByIdQuery.java
+++ src/main/java/com/github/m0ttii/database/orm/query/FindByIdQuery.java
@@ -5,13 +5,22 @@ import com.github.m0ttii.database.annotations.Id;
 import java.lang.reflect.Field;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 public class FindByIdQuery<T> extends BaseQuery<T> {
-    private final Object id;
+    private final Object idEntity;
+    private final Map<String, Object> compositeId;
 
-    public FindByIdQuery(Class<T> type, Object id) {
+    public FindByIdQuery(Class<T> type, Object idEntity) {
         super(type);
-        this.id = id;
+        this.idEntity = idEntity;
+        this.compositeId = null;
+    }
+
+    public FindByIdQuery(Class<T> type, Map<String, Object> compositeId) {
+        super(type);
+        this.idEntity = null;
+        this.compositeId = compositeId;
     }
 
     @Override
@@ -19,7 +28,6 @@ public class FindByIdQuery<T> extends BaseQuery<T> {
         String tableName = getTableName();
         Field[] fields = type.getDeclaredFields();
         StringBuilder columns = new StringBuilder();
-        String idColumn = null;
         StringBuilder joinClause = new StringBuilder();
 
         for (String join : joins) {
@@ -28,25 +36,27 @@ public class FindByIdQuery<T> extends BaseQuery<T> {
 
         for (Field field : fields) {
             columns.append(getColumnName(field)).append(",");
-            if (field.isAnnotationPresent(Id.class)) {
-                idColumn = getColumnName(field);
-                String x = idColumn;
-            }
         }
 
         columns.setLength(columns.length() - 1);
-        if (idColumn == null) {
-            throw new IllegalStateException("No ID column found");
-        }
-
-        conditions.put(idColumn, id);
 
         StringBuilder whereClause = new StringBuilder(" WHERE ");
-        for (String fieldName : conditions.keySet()) {
-            whereClause.append(fieldName).append(" = ? AND ");
+        if (hasCompositeKey()) {
+            try {
+                Map<String, Object> keyValues = compositeId != null ? compositeId : getCompositeKeyValues(idEntity);
+                for (Map.Entry<String, Object> entry : keyValues.entrySet()) {
+                    whereClause.append(entry.getKey()).append(" = ? AND ");
+                    conditions.put(entry.getKey(), entry.getValue());
+                }
+                whereClause.setLength(whereClause.length() - 5);
+            } catch (IllegalAccessException | NoSuchFieldException e) {
+                throw new RuntimeException("Failed to get composite key values", e);
+            }
+        } else {
+            String idColumn = getPrimaryKeyColumn(fields);
+            conditions.put(idColumn, idEntity);
+            whereClause.append(idColumn).append(" = ?");
         }
-        whereClause.setLength(whereClause.length() - 5);
-
 
         return "SELECT " + columns + " FROM " + tableName + " " + joinClause.toString() + whereClause.toString();
     }
